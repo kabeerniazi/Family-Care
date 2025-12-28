1cd78587e573d60ba7ec91f996ec66558dc50557
Muhammad Kabeer Khan <204548005+kabeerniazi@users.noreply.github.com>
2025-12-28 13:05:29 +0500

    Update static file serving path

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
